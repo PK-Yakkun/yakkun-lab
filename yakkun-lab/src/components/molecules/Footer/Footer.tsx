686c05742686eb74c815826b38ede592cd9d431b
PK-Yakkun <mokkun515@gmail.com>
2022-11-08 01:22:02 +0900

    feat: Footerコンポーネント改修

--- yakkun-lab/src/components/molecules/Footer/Footer.tsx
+++ yakkun-lab/src/components/molecules/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import { Box } from "@/components/atoms/Box/Box";
 export const Footer = () => {
   return (
     <Box bgColor="secondary">
-      <Stack justifyContent="center" py={1}>
+      <Stack justifyContent="center" pt={1} pb={18}>
         <Typography variant="caption">Development by Yakkun.</Typography>
       </Stack>
     </Box>
