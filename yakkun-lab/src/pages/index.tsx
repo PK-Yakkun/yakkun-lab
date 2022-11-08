0365b177c83063862592036ad5ff64fcbadd6c2c
PK-Yakkun <mokkun515@gmail.com>
2022-11-09 00:28:51 +0900

    feat: HOME画面作成

--- yakkun-lab/src/pages/index.tsx
+++ yakkun-lab/src/pages/index.tsx
@@ -1,82 +1,48 @@
-import { ExternalLinkIcon } from "@/components/atoms/Icons/ExternalLinkIcon";
+import { ArrowDownIcon } from "@/components/atoms/Icons/ArrowDownIcon";
 import { Stack } from "@/components/atoms/Stack/Stack";
 import { Typography } from "@/components/atoms/Typography/Typography";
-import { Counter } from "@/components/organisms/Counter/Counter";
-import { ThemeToggle } from "@/components/organisms/ThemeToggle/ThemeToggle";
+import { Accordion } from "@/components/organisms/Accordion/Accordion";
 import { NormalTemp } from "@/components/templetes/NormalTemp/NormalTemp";
 import type { NextPage } from "next";
-import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <NormalTemp>
-      <Stack justifyContent="center" mb={2} pr={1}>
-        <Image src="/images/logo_yuis.svg" width={220} height={80} alt="YUIS" />
-      </Stack>
-      <Typography variant="h2" textAlign="center" mb={12}>
-        - Yakkun UI System -
+      <Typography variant="title" mb={2}>
+        Yakkun Lab
       </Typography>
-      <Typography variant="h3" mb={3}>
-        YUISとは
+      <Typography variant="h2" mb={{ sm: 10, md: 10 }}>
+        Frontend and Design
       </Typography>
-      <Typography variant="body">
-        React,
-        Next.js環境下でUIコンポーネントライブラリを構築するための自作フレームワークです。
+      {/* Profile */}
+      <Typography variant="h3" pt={6}>
+        Profile
+      </Typography>
+      <Typography variant="body" mt={2}>
+        やっくんです。
         <br />
-        このページもYUISを用いて作成されており、おもな機能のサンプルを載せています。
+        フロントエンドやWebデザインをやったりやってなかったりしています。
         <br />
-        詳細は下記リポジトリのREADMEをご覧ください。細かい解説はZennに書いています。
+        Next.js, Typescriptをよく使います。
       </Typography>
-      <Stack>
-        <Typography mr={1}>Github:</Typography>
-        <Typography color="primary">
-          <Stack alignItems="center" gap="4px">
-            <a href="https://github.com/PK-Yakkun/YUIS" target="blank">
-              YUIS
-            </a>
-            <ExternalLinkIcon fill="primary" viewBox="0 0 24 24" width="18px" />
-          </Stack>
-        </Typography>
-      </Stack>
-      <Stack mb={8}>
-        <Typography mr={1}>Zenn:</Typography>
-        <Typography color="primary">
-          <Stack alignItems="center" gap="4px">
-            <a
-              href="https://zenn.dev/pk_yakkun/articles/54a8fcbcc0195e"
-              target="blank"
-            >
-              UIライブラリを簡単に構築するためのシステム『YUIS』を作成・公開しました
-            </a>
-            <ExternalLinkIcon fill="primary" viewBox="0 0 24 24" width="18px" />
-          </Stack>
-        </Typography>
-      </Stack>
-
-      <Stack justifyContent="space-between" alignItems="center" mb={3}>
-        <Typography variant="h3">テーマの切り替え</Typography>
-        <ThemeToggle />
-      </Stack>
-      <Typography variant="body" mb={8}>
-        右上のトグルスイッチでテーマを切り替えることができます。
+      <Typography variant="h3" mt={10}>
+        About
       </Typography>
-      <Typography variant="h3" mb={3}>
-        レスポンシブ
-      </Typography>
-      <Typography variant="body" mb={8}>
-        画面幅を変更することでPC/SP用のレイアウトが動的に変更されます。
-      </Typography>
-      <Typography variant="h3" mb={3}>
-        複合的なコンポーネント
-      </Typography>
-      <Typography variant="body" mb={3}>
-        サンプルで作成したカウンターです。
+      <Typography variant="body" mt={2} mb={4}>
+        アプリアイコンがメニューになっています。
         <br />
-        正と負の上限はそれぞれ10で、MAXに達すると該当するボタンが非活性になります。
-        <br />
-        機能をもつ複合的なコンポーネントは、Atomic Designの理解に最適です。
+        ギャラリーやUIライブラリを開いたり開かなかったりできます。
+        また、ナイトモードに切り替えたり切り変えなかったりもできます。
       </Typography>
-      <Counter />
+      <Accordion />
+      <Typography variant="h3" mt={10}>
+        Contact
+      </Typography>
+      <Typography variant="body" mt={2}>
+        制作などのご依頼はTwitterのDMでお願いいたします。
+        <br />
+        画面下のメニュー内、Twitterアイコンよりアクセスできます。
+      </Typography>
     </NormalTemp>
   );
 };
