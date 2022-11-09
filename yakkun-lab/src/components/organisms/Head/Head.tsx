e06c71b8dbcb5acea0721115d3b6f6292659b1f8
PK-Yakkun <mokkun515@gmail.com>
2022-11-09 21:34:55 +0900

    add: Headコンポーネント作成

---
+++ yakkun-lab/src/components/organisms/Head/Head.tsx
@@ -0,0 +1,15 @@
+import NextHead from "next/head";
+
+type HeadProps = {
+  title: string;
+  children?: React.ReactNode;
+};
+
+export const Head = ({ title, children }: HeadProps) => {
+  return (
+    <NextHead>
+      <title>Yakkun Lab｜{title}</title>
+      {children}
+    </NextHead>
+  );
+};
