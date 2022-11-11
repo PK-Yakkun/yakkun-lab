672f40d1b915035d93f771ac7e85780edf7f2cdd
PK-Yakkun <mokkun515@gmail.com>
2022-11-11 22:33:02 +0900

    fix: Footerコンポーネントにas="footer"追加

--- yakkun-lab/src/components/molecules/Footer/Footer.tsx
+++ yakkun-lab/src/components/molecules/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { Box } from "@/components/atoms/Box/Box";
 
 export const Footer = () => {
   return (
-    <Box bgColor="secondary">
+    <Box as="footer" bgColor="secondary">
       <Stack justifyContent="center" pt={1} pb={18}>
         <Typography variant="caption">Development by Yakkun.</Typography>
       </Stack>
