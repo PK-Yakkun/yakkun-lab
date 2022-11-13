9dfc64875fbd198dbefd7e41b4abfb88a0b02c9d
PK-Yakkun <mokkun515@gmail.com>
2022-11-13 23:38:38 +0900

    feat: positionMixinにz-index追加、レスポンシブに対応

--- yakkun-lab/src/styles/mixins/position.ts
+++ yakkun-lab/src/styles/mixins/position.ts
@@ -1,12 +1,15 @@
 import * as CSS from "csstype";
 import { css } from "@emotion/react";
+import { createResponsiveStyle } from "@/lib/responsive";
+import { BreakPointProps } from "@/types/responsive";
 
 export type PositionProps = {
-  position: CSS.Property.Position;
-  top: string;
-  right: string;
-  bottom: string;
-  left: string;
+  position: CSS.Property.Position | BreakPointProps;
+  top: string | BreakPointProps;
+  right: string | BreakPointProps;
+  bottom: string | BreakPointProps;
+  left: string | BreakPointProps;
+  zIndex: CSS.Property.ZIndex | BreakPointProps;
 };
 
 export const positionMixin = ({
@@ -15,12 +18,31 @@ export const positionMixin = ({
   right,
   bottom,
   left,
+  zIndex,
 }: Partial<PositionProps>) => {
   return css(
-    position != null && `position: ${position};`,
-    top != null && `top: ${top};`,
-    right != null && `right: ${right};`,
-    bottom != null && `bottom: ${bottom};`,
-    left != null && `left: ${left};`
+    position != null && typeof position === "string"
+      ? { position: position }
+      : typeof position === "object" &&
+          createResponsiveStyle("position", position.sm, position.md),
+    top != null && typeof top === "string"
+      ? { top: top }
+      : typeof top === "object" && createResponsiveStyle("top", top.sm, top.md),
+    right != null && typeof right === "string"
+      ? { right: right }
+      : typeof right === "object" &&
+          createResponsiveStyle("right", right.sm, right.md),
+    bottom != null && typeof bottom === "string"
+      ? { bottom: bottom }
+      : typeof bottom === "object" &&
+          createResponsiveStyle("bottom", bottom.sm, bottom.md),
+    left != null && typeof left === "string"
+      ? { left: left }
+      : typeof left === "object" &&
+          createResponsiveStyle("left", left.sm, left.md),
+    zIndex != null && typeof zIndex === "string"
+      ? { zIndex: zIndex }
+      : typeof zIndex === "object" &&
+          createResponsiveStyle("zIndex", zIndex.sm, zIndex.md)
   );
 };
