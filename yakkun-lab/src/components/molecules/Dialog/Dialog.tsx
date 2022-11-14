5d76f38f460154a2787e231b0c2d50fdf4d38dd8
PK-Yakkun <mokkun515@gmail.com>
2022-11-14 19:56:22 +0900

    add: Dialogコンポーネント作成

---
+++ yakkun-lab/src/components/molecules/Dialog/Dialog.tsx
@@ -0,0 +1,26 @@
+import { Box } from "@/components/atoms/Box/Box";
+import { Button } from "@/components/atoms/Button/Button";
+import { Screen } from "@/components/atoms/Screen/Screen";
+import { useState } from "react";
+
+export type DialogProps = {
+  switchItem: React.ReactNode;
+};
+
+export const Dialog = ({ switchItem }: DialogProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <>
+      <Box onClick={() => !setIsOpen} display="block" w="100%" h="100%">
+        {switchItem}
+      </Box>
+      <Box
+        display={isOpen ? "block" : "none"}
+        opacity={isOpen ? "1" : "0"}
+        transition=".3s"
+      >
+        <Screen isOpen={isOpen} setIsOpen={setIsOpen} />
+      </Box>
+    </>
+  );
+};
