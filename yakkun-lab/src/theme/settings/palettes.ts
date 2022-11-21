04d63b5fb70d81985c8a1faf718b8c3000ee7075
PK-Yakkun <mokkun515@gmail.com>
2022-11-21 22:38:58 +0900

    feat: アクセントカラー追加

--- yakkun-lab/src/theme/settings/palettes.ts
+++ yakkun-lab/src/theme/settings/palettes.ts
@@ -2,6 +2,7 @@ export const palette = {
   body: "#fff",
   primary: "#9a9fac",
   secondary: "#f3f3f3",
+  accent: "#8ab6e6",
   typography: "#5f626c",
 };
 
@@ -9,5 +10,6 @@ export const nightPalette = {
   body: "#202124",
   primary: "#50535a",
   secondary: "#ebebeb",
+  accent: "#86add7",
   typography: "#8b8f9d",
 };
