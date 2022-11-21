4e1e0a6f9a19de4b2d16da33a4577b7cf9b9ccb3
PK-Yakkun <mokkun515@gmail.com>
2022-11-22 00:07:16 +0900

    fix: 余白の調整

--- yakkun-lab/src/pages/design-system.tsx
+++ yakkun-lab/src/pages/design-system.tsx
@@ -12,34 +12,34 @@ const DesignSystem = () => {
       <Typography variant="h1" mb={{ sm: 4, md: 10 }}>
         Design System
       </Typography>
-      <Box py={6}>
+      <Box py={5}>
         <Typography variant="h2">Typography</Typography>
-        <List py={3} w="100%" overflow="scroll">
-          <ListItem py={2}>
+        <List py={4} w="100%" overflow="scroll">
+          <ListItem py={3}>
             <Typography variant="h3">title</Typography>
             <Typography variant="title" whiteSpace="nowrap">
               M+1p 900 Yakkun Love
             </Typography>
           </ListItem>
-          <ListItem py={2}>
+          <ListItem py={3}>
             <Typography variant="h3">h1</Typography>
             <Typography variant="h1" whiteSpace="nowrap">
               M+1p 700 Yakkun Love
             </Typography>
           </ListItem>
-          <ListItem py={2}>
+          <ListItem py={3}>
             <Typography variant="h3">h2</Typography>
             <Typography variant="h2" whiteSpace="nowrap">
               M+1p 700 Yakkun Love
             </Typography>
           </ListItem>
-          <ListItem py={2}>
+          <ListItem py={3}>
             <Typography variant="h3">h3</Typography>
             <Typography variant="h3" whiteSpace="nowrap">
               M+1p 700 Yakkun Love
             </Typography>
           </ListItem>
-          <ListItem py={2}>
+          <ListItem py={3}>
             <Typography variant="h3">body</Typography>
             <Typography variant="body" whiteSpace="nowrap">
               M+1p 400 Yakkun Love
@@ -53,7 +53,7 @@ const DesignSystem = () => {
           </ListItem>
         </List>
       </Box>
-      <Box py={6}>
+      <Box py={5}>
         <Typography variant="h2">Colors</Typography>
         <Stack py={4} gap="30px" overflow="scroll">
           <Box bgColor="body" w="50px" h="50px" border="1px solid #9a9fac" />
@@ -63,56 +63,98 @@ const DesignSystem = () => {
           <Box bgColor="typography" w="50px" h="50px" />
         </Stack>
       </Box>
-      <Box py={6}>
+      <Box py={5}>
         <Typography variant="h2">Spacing</Typography>
         <Stack py={4} gap="30px" overflow="scroll">
-          <Stack direction="column" justifyContent="center" alignItems="center">
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
             <Typography variant="caption" color="accent">
               2px
             </Typography>
             <Box bgColor="accent" w="2px" h="2px" opacity="0.4" />
           </Stack>
-          <Stack direction="column" justifyContent="center" alignItems="center">
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
             <Typography variant="caption" color="accent">
               6px
             </Typography>
             <Box bgColor="accent" w="6px" h="6px" opacity="0.4" />
           </Stack>
-          <Stack direction="column" justifyContent="center" alignItems="center">
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
             <Typography variant="caption" color="accent">
               12px
             </Typography>
             <Box bgColor="accent" w="12px" h="12px" opacity="0.4" />
           </Stack>
-          <Stack direction="column" justifyContent="center" alignItems="center">
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
             <Typography variant="caption" color="accent">
               18px
             </Typography>
             <Box bgColor="accent" w="18px" h="18px" opacity="0.4" />
           </Stack>
-          <Stack direction="column" justifyContent="center" alignItems="center">
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
             <Typography variant="caption" color="accent">
               24px
             </Typography>
             <Box bgColor="accent" w="24px" h="24px" opacity="0.4" />
           </Stack>
-          <Stack direction="column" justifyContent="center" alignItems="center">
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
             <Typography variant="caption" color="accent">
               30px
             </Typography>
             <Box bgColor="accent" w="30px" h="30px" opacity="0.4" />
           </Stack>
-          <Stack direction="column" justifyContent="center" alignItems="center">
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
             <Typography variant="caption" color="accent">
               36px
             </Typography>
             <Box bgColor="accent" w="36px" h="36px" opacity="0.4" />
           </Stack>
-          <Stack direction="column" justifyContent="center" alignItems="center">
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
             <Typography variant="caption" color="accent">
               42px
             </Typography>
             <Box bgColor="accent" w="42px" h="42px" opacity="0.4" />
+          </Stack>
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
+            <Typography variant="caption" color="accent">
+              (n × 6)px ...
+            </Typography>
+            <Box bgColor="accent" w="82px" h="42px" opacity="0.4" />
           </Stack>
         </Stack>
       </Box>
