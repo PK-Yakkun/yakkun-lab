5a3a6e84c7c3fa3a66c2ec0022116928ddcbbd8a
PK-Yakkun <mokkun515@gmail.com>
2022-11-27 15:34:36 +0900

    Revert "feat: GitHubから草取得したかった"
    This reverts commit ef45e2206a54d7d19d204f1f464d1df88f6a96eb.

--- yakkun-lab/src/pages/_app.tsx
+++ yakkun-lab/src/pages/_app.tsx
@@ -8,8 +8,6 @@ import { Head } from "@/components/organisms/Head/Head";
 import { Menu } from "@/components/organisms/Menu/Menu";
 import { NormalTemp } from "@/components/templetes/NormalTemp/NormalTemp";
 import { Footer } from "@/components/molecules/Footer/Footer";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { githubData } from "@/api/github";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // NightModeに切り替えるステート
@@ -17,47 +15,45 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <ApolloProvider client={githubData}>
-        <ThemeProvider theme={isNightMode ? nightTheme : theme}>
-          <ThemeContext.Provider value={{ isNightMode, setIsNightMode }}>
-            <Head>
-              <meta charSet="utf-8" />
-              <meta
-                name="viewport"
-                content="initial-scale=1.0, width=device-width"
-              />
-              <meta
-                property="description"
-                content="やっくんのポートフォリオです。Webデザインとかフロントエンドとかやったりやってなかったりします。"
-              />
-              <meta property="og:title" content="Yakkun Lab" />
-              <meta
-                property="og:description"
-                content="やっくんのポートフォリオです。Webデザインとかフロントエンドとかやったりやってなかったりします。"
-              />
-              <meta
-                property="og:image"
-                content={`https://pk-yakkun.com/images/og/ogp_l_yakkun-lab.png`}
-              />
-              <meta name="twitter:card" content="summary_large_image" />
-              <meta name="twitter:title" content="Yakkun Lab" />
-              <meta
-                name="twitter:description"
-                content="やっくんのポートフォリオです。"
-              />
-              <meta
-                name="twitter:image"
-                content="https://pk-yakkun.com/images/og/ogp_l_yakkun-lab.png"
-              />
-            </Head>
-            <NormalTemp>
-              <Component {...pageProps} />
-            </NormalTemp>
-            <Footer />
-            <Menu />
-          </ThemeContext.Provider>
-        </ThemeProvider>
-      </ApolloProvider>
+      <ThemeProvider theme={isNightMode ? nightTheme : theme}>
+        <ThemeContext.Provider value={{ isNightMode, setIsNightMode }}>
+          <Head>
+            <meta charSet="utf-8" />
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+            <meta
+              property="description"
+              content="やっくんのポートフォリオです。Webデザインとかフロントエンドとかやったりやってなかったりします。"
+            />
+            <meta property="og:title" content="Yakkun Lab" />
+            <meta
+              property="og:description"
+              content="やっくんのポートフォリオです。Webデザインとかフロントエンドとかやったりやってなかったりします。"
+            />
+            <meta
+              property="og:image"
+              content={`https://pk-yakkun.com/images/og/ogp_l_yakkun-lab.png`}
+            />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content="Yakkun Lab" />
+            <meta
+              name="twitter:description"
+              content="やっくんのポートフォリオです。"
+            />
+            <meta
+              name="twitter:image"
+              content="https://pk-yakkun.com/images/og/ogp_l_yakkun-lab.png"
+            />
+          </Head>
+          <NormalTemp>
+            <Component {...pageProps} />
+          </NormalTemp>
+          <Footer />
+          <Menu />
+        </ThemeContext.Provider>
+      </ThemeProvider>
     </>
   );
 }
--- yakkun-lab/src/pages/index.tsx
+++ yakkun-lab/src/pages/index.tsx
@@ -2,64 +2,53 @@ import { Typography } from "@/components/atoms/Typography/Typography";
 import { Accordion } from "@/components/organisms/Accordion/Accordion";
 import { Head } from "@/components/organisms/Head/Head";
 import type { NextPage } from "next";
-import { useQuery } from "@apollo/client";
-import { GET_CURRENT_USER } from "@/api/github";
-import { Box } from "@/components/atoms/Box/Box";
-import { ContributionCalendar } from "@/components/organisms/ContributionCalendar";
 
 const Home: NextPage = () => {
-  const datas = useQuery(GET_CURRENT_USER);
-  // const a =
-  //   datas!.data!.user!.contributionsCollection!.contributionCalendar!.weeks!
-  //     .contributionDays;
-  // console.log(datas);
-
   return (
-    datas && (
-      <>
-        <Head title="Home" />
-        <Typography variant="title" mb={2}>
-          Yakkun Lab
-        </Typography>
-        <Typography variant="h2" mb={{ sm: 6, md: 10 }}>
-          Frontend and Design
-        </Typography>
-        {/* Profile */}
-        <Typography variant="h3" pt={6}>
-          Profile
-        </Typography>
-        <Typography variant="body" mt={2}>
-          やっくんです。
-          <br />
-          フロントエンドやWebデザインをやったりやってなかったりしています。
-          <br />
-          Next.js, Typescriptをよく使います。
-        </Typography>
+    <>
+      <Head title="Home" />
+      <Typography variant="title" mb={2}>
+        Yakkun Lab
+      </Typography>
+      <Typography variant="h2" mb={{ sm: 6, md: 10 }}>
+        Frontend and Design
+      </Typography>
 
-        <ContributionCalendar data={datas} />
-        {/* About */}
-        <Typography variant="h3" mt={{ sm: 8, md: 10 }}>
-          About
-        </Typography>
-        <Typography variant="body" mt={2} mb={4}>
-          アプリアイコンがメニューになっています。
-          <br />
-          ギャラリーや各SNSを開いたり開かなかったりできます。
-          <br />
-          また、ナイトモードに切り替えたり切り替えなかったりもできます。
-        </Typography>
-        <Accordion />
-        {/* Contact */}
-        <Typography variant="h3" mt={{ sm: 8, md: 10 }}>
-          Contact
-        </Typography>
-        <Typography variant="body" mt={2}>
-          制作などのご依頼はTwitterのDMでお願いいたします。
-          <br />
-          画面下のメニュー内、Twitterアイコンよりアクセスできます。
-        </Typography>
-      </>
-    )
+      {/* Profile */}
+      <Typography variant="h3" pt={6}>
+        Profile
+      </Typography>
+      <Typography variant="body" mt={2}>
+        やっくんです。
+        <br />
+        フロントエンドやWebデザインをやったりやってなかったりしています。
+        <br />
+        Next.js, Typescriptをよく使います。
+      </Typography>
+
+      {/* About */}
+      <Typography variant="h3" mt={{ sm: 8, md: 10 }}>
+        About
+      </Typography>
+      <Typography variant="body" mt={2} mb={4}>
+        アプリアイコンがメニューになっています。
+        <br />
+        ギャラリーや各SNSを開いたり開かなかったりできます。
+        <br />
+        また、ナイトモードに切り替えたり切り替えなかったりもできます。
+      </Typography>
+      <Accordion />
+
+      {/* Contact */}
+      <Typography variant="h3" mt={{ sm: 8, md: 10 }}>
+        Contact
+      </Typography>
+      <Typography variant="body" mt={2}>
+        制作などのご依頼はTwitterのDMでお願いいたします。
+        <br />
+        画面下のメニュー内、Twitterアイコンよりアクセスできます。
+      </Typography>
+    </>
   );
 };
 
